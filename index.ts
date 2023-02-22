6983f6a0c42c06b3325f7273e477654aff017b4c
itai <itaimorag2000@gmail.com>
2023-02-22 15:08:54 +0200

    render doing problems

--- index.ts
+++ index.ts
@@ -6,18 +6,28 @@ import { Server, Socket } from "socket.io";
 import { generateFlightNumber } from "./utils";
 import { airports } from "./airportList";
 import moment from "moment";
-
+const path = require('path')
 // const PORT = 3030;
 const TIME_FORMAT = "DD/MM/yyyy - HH:mm";
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true
-  })
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+      origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:5174', 'http://localhost:5174'],
+      credentials: true
+  }
+  app.use(cors(corsOptions))
+}
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true
+//   })
+// );
 
 const server = http.createServer(app);
 
